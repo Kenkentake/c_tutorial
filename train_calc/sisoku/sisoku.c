114ca071b2e18f0b57bb8f74bd6f4cfb6b37e743
竹本健 <takemotoken@KENs-MacBook-Air.local>
2020-08-18 23:19:44 +0900

    add-files

---
+++ train_calc/sisoku/sisoku.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+    演算子を用いた計算のプログラム
+*/
+
+void main()
+{
+    // 各種計算
+    printf("%d + %d = %d\n",5,2,5+2);
+    printf("%d - %d = %d\n",5,2,5-2);
+    printf("%d * %d = %d\n",5,2,5*2);
+    printf("%d / %d = %d ... %d \n",5,2,5/2, 5%2);
+}
