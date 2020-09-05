cd015bd0231fd131d4205678df0461e5146a9a18
Kenkentake <keeeentake0908@gmail.com>
2020-09-05 23:49:06 +0900

    add advanced

---
+++ basic/train_if/switch/switch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+    int num;
+    printf("1~3の値を入力してください");
+    scanf("%d",&num);
+    switch(num){
+        case 1:
+            printf("one\n");
+            break;
+        case 2:
+            printf("two\n");
+            break;
+        default:
+            printf("不適切な値です。\n");
+            break; 
+    }
+}
