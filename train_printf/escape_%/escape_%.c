4d0982cbee440366dd2d6fa5f59ad98bb53e39e0
Kenkentake <keeeentake0908@gmail.com>
2020-09-07 00:05:35 +0900

    rm train-dir

--- train_printf/escape_%/escape_%.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-    printf("Hello,my name is %s. \nI'm %d years old.\n","Ken Takemoto",23);
-    printf("My initial is %c.\n", 'K');
-    printf("%f + %f = %f\n", 1.2,2.7,1.2 +2.7);
-}
--- train_printf/escape_sequence/escape_sequence.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void main(){
-    printf("123\n456\n789\n");
-    printf("シングルクォーテーション:\ダブルクォーテーション:\"\n");
-    printf("\t円マーク\\\n");
-
-}
